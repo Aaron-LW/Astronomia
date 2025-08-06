86eb189c137dfcd75bee077c16c88fd224540c59
linskiv1 <lianbenweber@gmail.com>
2025-08-06 15:36:26 +0200

    HI

--- Calvini_Bombini/Code/Game1.cs
+++ Calvini_Bombini/Code/Game1.cs
@@ -83,7 +83,7 @@ public class Game1 : Game
         //Noch ein test weil hoffnung
 
         _spriteBatch.End();
-
+        //HI                                                                                                  
         base.Draw(gameTime);
     }
 }
