fd17c4708c26fddd5bc76473e57c4dfbdb0f0ea0
Aaron_LW <aaronl.waechter@gmail.com>
2025-08-11 17:04:07 +0200

    made the camera always focused on the player

--- Calvini_Bombini/Code/Statics/Camera.cs
+++ Calvini_Bombini/Code/Statics/Camera.cs
@@ -11,7 +11,7 @@ public static class Camera
 
     public static void Start()
     {
-        //FocusedEntity = EntitySystem.Player;
+        FocusedEntity = EntitySystem.Player;
     }
 
     public static void Update()
@@ -47,7 +47,7 @@ public static class Camera
 
             if (positionComponent != null)
             {
-                SetPosition(positionComponent.Position);
+                SetPosition(positionComponent.Position + (new Vector2(FocusedEntity.Texture.Width / 2, FocusedEntity.Texture.Height / 2) * Settings.GlobalScale) - (ViewportBounds.Bounds / (2f * Zoom)));
             }
         }
     }
--- Calvini_Bombini/Code/Statics/Settings.cs
+++ Calvini_Bombini/Code/Statics/Settings.cs
@@ -1,5 +1,5 @@
 public static class Settings
 {
-    public static readonly float GlobalScale = 3f;
+    public static readonly float GlobalScale = 1f;
     public static readonly float CameraSpeed = 500f;
 }
